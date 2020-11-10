4c106d5965bad49fa8c4584066895511a1406b14
carlo <carlo.vanderstoep@gmail.com>
2020-11-10 16:58:11 +0100

    added unit tests for check bracket

---
+++ week1_basic_data_structures/1_brackets_in_code/check_bracket_unit_tests.py
@@ -0,0 +1,17 @@
+import unittest
+import check_brackets
+
+
+class TestCheckBrackets(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(check_brackets.find_mismatch('[]'), "Success")
+        self.assertEqual(check_brackets.find_mismatch('{}[]'), "Success")
+        self.assertEqual(check_brackets.find_mismatch('[{}]'), "Success")
+        self.assertEqual(check_brackets.find_mismatch('[](){}'), "Success")
+        self.assertEqual(check_brackets.find_mismatch('{[}'), (3, False))
+        self.assertEqual(check_brackets.find_mismatch('[[]}]{}'), (4, False))
+        self.assertEqual(check_brackets.find_mismatch('foo(bar[i);'), (10, False))
+
+
+if __name__ == '__main__':
+    unittest.main()
--- week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 # below named tuple is used to put both the character and position on the stack
 Bracket = namedtuple("Bracket", ["char", "position"])
 
+
 # below method checks for matching left & right character and return True if matching
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
@@ -12,18 +13,20 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
-        br = Bracket(next, i)
-        print("bracket is: ", br)
-        if next in "([{":
+    for i, next_character in enumerate(text):
+        br = Bracket(next_character, i)
+        # print("bracket is: ", br)
+        if next_character in "([{":
             # Process opening bracket
             opening_brackets_stack.append(br)
 
-        if next in ")]}":
+        if next_character in ")]}":
             # Process closing bracket, write your code here
-            if len(opening_brackets_stack) == 0: return i + 1, False
+            if len(opening_brackets_stack) == 0:
+                return i + 1, False
             top_of_stack_char, top_of_stack_pos = opening_brackets_stack.pop()
-            if not are_matching(top_of_stack_char, next): return i + 1, False
+            if not are_matching(top_of_stack_char, next_character):
+                return i + 1, False
 
     # everything matches and stack is empty
     if len(opening_brackets_stack) == 0:
