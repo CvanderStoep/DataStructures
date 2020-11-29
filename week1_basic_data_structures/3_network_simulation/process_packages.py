7d65b394297b21bbbdaa7f1d28220600905212a0
carlo <carlo.vanderstoep@gmail.com>
2020-11-29 17:28:38 +0100

    initial commit network simulation

--- week1_basic_data_structures/3_network_simulation/process_packages.py
+++ week1_basic_data_structures/3_network_simulation/process_packages.py
@@ -12,19 +12,41 @@ class Buffer:
         self.finish_time = []
 
     def process(self, request):
-        # write your code here
-        return Response(False, -1)
+        if len(self.finish_time) == 0:
+            latest_finish_time = 0
+        else:
+            latest_finish_time = self.finish_time[-1]  # the last element of the list
+
+        new_finish_time = max(request.arrived_at, latest_finish_time) + request.time_to_process
+
+        # remove all elements that are smaller than the new request
+        # only clear the buffer when you have to
+        if len(self.finish_time) > self.size - 2:
+            self.finish_time = [time for time in self.finish_time if time > request.arrived_at]
+
+        if len(self.finish_time) < self.size:
+            self.finish_time.append(new_finish_time)
+            start_processing = max(latest_finish_time, request.arrived_at)
+            return Response(False, start_processing)
+        else:
+            return Response(True, -1)
 
 
 def process_requests(requests, buffer):
     responses = []
+    teller = 0
     for request in requests:
+        teller += 1
+        if teller % 1000 == 0: print('teller: ', teller)
         responses.append(buffer.process(request))
     return responses
 
 
 def main():
-    buffer_size, n_requests = map(int, input().split())
+    # TODO run all files in test directory
+    # write output naar file.b
+    # use filecmp.cmp(file.a, file.b) to check if they are equal
+    buffer_size, n_requests = map(int, input("buffer size, number of packets:\n").split())
     requests = []
     for _ in range(n_requests):
         arrived_at, time_to_process = map(int, input().split())
