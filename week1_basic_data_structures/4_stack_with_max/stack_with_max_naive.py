c9e33cc8d52e1daeb99fbb54bfb7fc66b1be422c
carlo <carlo.vanderstoep@gmail.com>
2020-12-01 15:05:08 +0100

    example input lines in main()

--- week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
+++ week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
@@ -10,11 +10,15 @@ class StackWithMaxNaive():
         self.__stack.append(a)
 
     def Pop(self):
-        assert (len(self.__stack))
+        if len(self.__stack) == 0:
+            print('stack contains no elements')
+            return
+        # assert (len(self.__stack))
         self.__stack.pop()
 
     def Max(self):
-        assert (len(self.__stack))
+        if len(self.__stack) == 0: return 'stack contains no elements'
+        # assert (len(self.__stack))
         return max(self.__stack)
 
 
@@ -33,30 +37,51 @@ class StackWithMax():
 
     def Pop(self):
         # if the maximum is removed from the stack, remove it also from auxiliary stack.
-        assert (len(self.__stack))
+        if len(self.__stack) == 0:
+            print('stack contains no elements')
+            return
+        # assert (len(self.__stack))
         if self.__stack[-1] == self.__maxstack[-1]:
             self.__maxstack.pop()
         self.__stack.pop()
 
     def Max(self):
         if len(self.__maxstack) == 0: return 'stack contains no elements'
-        assert len(self.__maxstack), 'contains no elements'
+        # assert len(self.__maxstack), 'contains no elements'
         return self.__maxstack[-1]
 
 
+# TODO write stress-test with randomized input
+# stacknaive and stack should give same output
+
 if __name__ == '__main__':
+    input_lines = ["push 2", "push 1", "max", "pop", "max", "pop", "pop", "push 8", "max"]
+    n = len(input_lines)
+
     # stack = StackWithMaxNaive()
     stack = StackWithMax()
 
-    num_queries = int(sys.stdin.readline())
-    for _ in range(num_queries):
-        query = sys.stdin.readline().split()
+    for _ in range(n):
+        q = input_lines.pop(0).split()
 
-        if query[0] == "push":
-            stack.Push(int(query[1]))
-        elif query[0] == "pop":
+        if q[0] == "push":
+            stack.Push(int(q[1]))
+        elif q[0] == "pop":
             stack.Pop()
-        elif query[0] == "max":
+        elif q[0] == "max":
             print(stack.Max())
         else:
             assert 0
+
+    # num_queries = int(sys.stdin.readline())
+    # for _ in range(num_queries):
+    #     query = sys.stdin.readline().split()
+    #
+    #     if query[0] == "push":
+    #         stack.Push(int(query[1]))
+    #     elif query[0] == "pop":
+    #         stack.Pop()
+    #     elif query[0] == "max":
+    #         print(stack.Max())
+    #     else:
+    #         assert 0
