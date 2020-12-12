90e89b80ef9e709b569a7221cd5bf5d9821c0467
carlo <carlo.vanderstoep@gmail.com>
2020-12-12 21:04:34 +0100

    initial commit for job queue using minHeap structure

--- week2_priority_queues_and_disjoint_sets/1_make_heap/Heap.py
+++ week2_priority_queues_and_disjoint_sets/1_make_heap/Heap.py
@@ -1,75 +1,76 @@
 import sys
 
+class Max_Heap_index1_based:
+    # index 1 based
+    def __init__(self):
+        self.array = ['dummy']  # dummy element to use an array starting at index 1
+        # self.size = len(self.array)
 
-# class Max_Heap:
-# # index 1 based
-#     def __init__(self):
-#         self.array = ['dummy']  # dummy element to use an array starting at index 1
-#         # self.size = len(self.array)
-#
-#     # return the node number of the Parent
-#     def Parent(self, i):
-#         return i // 2
-#
-#     def Size(self):
-#         return len(self.array) - 1
-#
-#     # return the node number of the left child
-#     def LeftChild(self, i):
-#         return 2 * i
-#
-#     # return the node number of the right child
-#     def RightChild(self, i):
-#         return 2 * i + 1
-#
-#     def SiftUp(self, i):
-#         while i > 1 and self.array[self.Parent(i)] < self.array[i]:
-#             self.array[self.Parent(i)], self.array[i] = self.array[i], self.array[self.Parent(i)]
-#             i = self.Parent(i)
-#
-#     def SiftDown(self, i):
-#         maxIndex = i
-#         l = self.LeftChild(i)
-#         if l <= self.Size() and self.array[l] > self.array[maxIndex]:
-#             maxIndex = l
-#         r = self.RightChild(i)
-#         if r <= self.Size() and self.array[r] > self.array[maxIndex]:
-#             maxIndex = r
-#         if i != maxIndex:
-#             self.array[i], self.array[maxIndex] = self.array[maxIndex], self.array[i]
-#             self.SiftDown(maxIndex)
-#
-#     def Insert(self, p):
-#         self.array.append(p)
-#         self.SiftUp(self.Size())
-#
-#     def ExtractMax(self):
-#         result = self.array[1]
-#         self.array[1] = self.array[self.Size()]
-#         self.array.pop()
-#         self.SiftDown(1)
-#         return result
-#
-#     def Remove(self, i):
-#         self.array[i] = sys.maxsize
-#         self.SiftUp(i)
-#         self.ExtractMax()
-#
-#     def ChangePriority(self, i, p):
-#         oldp = self.array[i]
-#         self.array[i] = p
-#         if p > oldp:
-#             self.SiftUp(i)
-#         else:
-#             self.SiftDown(i)
-#
-#     def BuildHeap(self, new_array):
-#         self.array += new_array
-#         for i in range(self.Size() // 2, 0, -1):
-#             self.SiftDown(i)
+    # return the node number of the Parent
+    def Parent(self, i):
+        return i // 2
+
+    def Size(self):
+        return len(self.array) - 1
+
+    # return the node number of the left child
+    def LeftChild(self, i):
+        return 2 * i
+
+    # return the node number of the right child
+    def RightChild(self, i):
+        return 2 * i + 1
+
+    def SiftUp(self, i):
+        while i > 1 and self.array[self.Parent(i)] < self.array[i]:
+            self.array[self.Parent(i)], self.array[i] = self.array[i], self.array[self.Parent(i)]
+            i = self.Parent(i)
+
+    def SiftDown(self, i):
+        maxIndex = i
+        l = self.LeftChild(i)
+        if l <= self.Size() and self.array[l] > self.array[maxIndex]:
+            maxIndex = l
+        r = self.RightChild(i)
+        if r <= self.Size() and self.array[r] > self.array[maxIndex]:
+            maxIndex = r
+        if i != maxIndex:
+            self.array[i], self.array[maxIndex] = self.array[maxIndex], self.array[i]
+            self.SiftDown(maxIndex)
+
+    def Insert(self, p):
+        self.array.append(p)
+        self.SiftUp(self.Size())
+
+    def ExtractMax(self):
+        result = self.array[1]
+        self.array[1] = self.array[self.Size()]
+        self.array.pop()
+        self.SiftDown(1)
+        return result
+
+    def Remove(self, i):
+        self.array[i] = sys.maxsize
+        self.SiftUp(i)
+        self.ExtractMax()
+
+    def ChangePriority(self, i, p):
+        oldp = self.array[i]
+        self.array[i] = p
+        if p > oldp:
+            self.SiftUp(i)
+        else:
+            self.SiftDown(i)
+
+    def BuildHeap(self, new_array):
+        self.array += new_array
+        for i in range(self.Size() // 2, 0, -1):
+            self.SiftDown(i)
 
 
 class Max_Heap:
+    """"This class implements the maxHeap Data structure."""
+
     # index 0 based
     def __init__(self):
         self.array = []
@@ -254,7 +255,12 @@ def main():
     print('building second minHeap ...')
     minHeap = Min_Heap()
     print('swaps: ', minHeap.BuildHeap([7, 8, 3, 1, 5, 4, 3, 2, 1]))
-    print(minHeap.array)
+    print('heap: ', minHeap.array)
+
+    print('building third minHeap with tuples...')
+    minHeap = Min_Heap()
+    print('swaps: ', minHeap.BuildHeap([(7, 1), (8, 5), (3, 7), (1, 4), (5, 6), (4, 10), (3, 7), (2, 1), (1, 5)]))
+    print('heap: ', minHeap.array)
 
 
 if __name__ == "__main__":
--- week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
+++ week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
@@ -1,12 +1,13 @@
 # python3
 
+import Heap
 from collections import namedtuple
 
 AssignedJob = namedtuple("AssignedJob", ["worker", "started_at"])
 
 
-def assign_jobs(n_workers, jobs):
-    # TODO: replace this code with a faster algorithm.
+def assign_jobs_naive(n_workers, jobs):
+    # DONE: replace this code with a faster algorithm.
     result = []
     next_free_time = [0] * n_workers
     for job in jobs:
@@ -17,13 +18,47 @@ def assign_jobs(n_workers, jobs):
     return result
 
 
+def assign_jobs(n_workers, jobs):
+    result = []
+
+    # create a workers minHeap with all workers available at time == 0
+    minHeap = Heap.Min_Heap()
+    for i in range(n_workers):
+        minHeap.Insert((0, i))  # insert the tuple (next_available_time, worker)
+
+    # check all jobs, get the first available worker from the Heap and add the new time to the Heap
+    for job in jobs:
+        time, worker = minHeap.ExtractMin()
+        # print('w,t: ', worker, time)
+        result.append(AssignedJob(worker, time))
+        minHeap.Insert((time + job, worker))
+
+    return result
+
+
 def main():
-    n_workers, n_jobs = map(int, input().split())
+    # example input:
+    # 2 5
+    # 1 2 3 4 5
+    # output:
+    # 0 0
+    # 1 0
+    # 0 1
+    # 1 2
+    # 0 4
+    n_workers, n_jobs = map(int, input('give input configuration: \n').split())
     jobs = list(map(int, input().split()))
     assert len(jobs) == n_jobs
 
-    assigned_jobs = assign_jobs(n_workers, jobs)
+    # using the naive method
+    print('using naive method ...')
+    assigned_jobs = assign_jobs_naive(n_workers, jobs)
+    for job in assigned_jobs:
+        print(job.worker, job.started_at)
 
+    print('using minHeap method ...')
+    # using the minHeap method
+    assigned_jobs = assign_jobs(n_workers, jobs)
     for job in assigned_jobs:
         print(job.worker, job.started_at)
 
