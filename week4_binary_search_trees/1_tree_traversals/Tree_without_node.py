a7f1691df2a731593cdc3d7609ba8358cd347908
carlo <carlo.vanderstoep@gmail.com>
2021-02-07 12:13:47 +0100

    Add BST implementation without nodes

---
+++ week4_binary_search_trees/1_tree_traversals/Tree_without_node.py
@@ -0,0 +1,61 @@
+class Tree:
+    def __init__(self, value=None):
+        self.value = value
+        self.left_child = None
+        self.right_child = None
+
+    def insert(self, value):
+        if self.value is None:
+            self.value = value
+            return
+        else:
+            if value < self.value:
+                if self.left_child is None:
+                    self.left_child = Tree(value)
+                else:
+                    self.left_child.insert(value)
+            elif value > self.value:
+                if self.right_child is None:
+                    self.right_child = Tree(value)
+                else:
+                    self.right_child.insert(value)
+            else:
+                print("Value is already existing in Tree.")
+
+    def print_tree(self):
+        #  in-order traversal, returns sorted elements of the BST
+        result = []
+
+        def in_order_traversal(node):
+            #  helper function for the recursion
+            if node.value is not None:
+                if node.left_child is not None:
+                    in_order_traversal(node.left_child)
+                result.append(node.value)
+                if node.right_child is not None:
+                    in_order_traversal(node.right_child)
+
+        in_order_traversal(self)
+        return result
+
+
+def main():
+    tree = Tree()
+    tree.insert(10)
+    tree.insert(5)
+    tree.insert(20)
+    tree.insert(15)
+    tree.insert(9)
+    tree.insert(14)
+    tree.insert(7)
+    tree.insert(23)
+    tree.insert(45)
+    tree.insert(91)
+
+    print(tree.print_tree())
+
+    print("done")
+
+
+if __name__ == "__main__":
+    main()
