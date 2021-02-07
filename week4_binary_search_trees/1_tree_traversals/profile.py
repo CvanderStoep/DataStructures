07a3c9808841c9d8f75af9f63f05d2fb5e4f0e81
Lex van der Stoep <lex.vanderstoep@imc.com>
2021-02-07 16:33:13 +0100

    Add profiling method to BSTs
    This helps analysing where the code spends most of its time.

---
+++ week4_binary_search_trees/1_tree_traversals/profile.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from pycallgraph import PyCallGraph
+from pycallgraph.output import GraphvizOutput
+
+from Tree_without_node import Tree
+
+graphviz = GraphvizOutput(output_file='filter_none.png')
+
+with PyCallGraph(output=graphviz):
+    tree = Tree()
+    for i in range(1,100):
+        tree.insert(i)
